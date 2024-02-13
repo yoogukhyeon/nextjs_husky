15bdd2d40878f189f55be3bf95f16affc42cfe36
yoogukhyeon <rnrgus5897@gmail.com>
2024-02-13 09:27:27 +0900

    last test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  let qq = '2'
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
